faf45cdc9145fc2e2b486f0d1ad848a88d127916
klim0v <klim0v-sergey@yandex.ru>
2019-03-02 00:57:58 +0300

    check basic features and move

---
+++ go.mod
@@ -0,0 +1,12 @@
+module imap-check
+
+go 1.12
+
+require (
+	github.com/emersion/go-imap v1.0.0-beta.1
+	github.com/emersion/go-imap-move v0.0.0-20180601155324-5eb20cb834bf
+	github.com/emersion/go-message v0.9.1 // indirect
+	github.com/emersion/go-sasl v0.0.0-20161116183048-7e096a0a6197 // indirect
+	github.com/emersion/go-textwrapper v0.0.0-20160606182133-d0e65e56babe // indirect
+	golang.org/x/text v0.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/emersion/go-imap"
+	"github.com/emersion/go-imap-move"
+	"github.com/emersion/go-imap/client"
+	"log"
+	"os"
+)
+
+func main() {
+	log.Println("Connecting to server...")
+
+	c, err := client.DialTLS("imap.yandex.ru:993", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected")
+	defer c.Logout()
+
+	if err := c.Login(os.Getenv("USERNAME"), os.Getenv("PASSWORD")); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Logged in")
+
+	criteria := imap.NewSearchCriteria()
+	_, err = c.Select("INBOX", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	criteria.Header.Add("From", `accounts@firefox.com`)
+	seqNums, err := c.Search(criteria)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(seqNums)
+
+	if len(seqNums) > 0 {
+		seqset := new(imap.SeqSet)
+		seqset.AddNum(seqNums...)
+
+		err = move.NewClient(c).Move(seqset, "Удаленные")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
